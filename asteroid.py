3c19460811cfffe7aa79292363db4a86c6bdfcdd
Gridec-dev <Gridec@icloud.com>
2025-07-29 17:05:33 -0400

    added asteroids being split instead of just disappearing

--- asteroid.py
+++ asteroid.py
@@ -1,14 +1,13 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
 
+rand_num = random.uniform(20,50)
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x,y,radius)
-        self.x = x
-        self.y = y
-        self.radius = radius
         
        
     def draw(self,screen):
@@ -21,3 +20,11 @@ class Asteroid(CircleShape):
         self.kill()
         if self.radius <+ ASTEROID_MIN_RADIUS:
             return
+        else:
+            rand_angle = self.velocity.rotate(rand_num)
+            neg_rand_angle = self.velocity.rotate(rand_num * -1)
+            small_asteroid = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_one = Asteroid(self.position.x,self.position.y,small_asteroid)
+            asteroid_two = Asteroid(self.position.x,self.position.y,small_asteroid)
+            asteroid_one.velocity = rand_angle * 1.2
+            asteroid_two.velocity = neg_rand_angle *1.2
